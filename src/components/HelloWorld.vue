8dd9463593780fd484e580ac66513c61b69c8927
Akbar Hazura <akbarhazura@gmail.com>
2021-12-21 21:18:00 +0700

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -40,7 +40,10 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="postcss">
+.hello{
+  @apply bg-black
+}
 h3 {
   margin: 40px 0 0;
 }
